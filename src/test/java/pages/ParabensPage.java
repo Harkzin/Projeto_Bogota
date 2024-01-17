53f2c7d3ce910eb5c16eff8b31b905302b709322
nicolas.yoshioka.glo <nicolas.yoshioka.3@globalhitss.com.br>
2024-01-17 09:54:58 -0300

    ECCM-3680 ajuste parabens e add cenario clearsale

--- src/test/java/pages/ParabensPage.java
+++ src/test/java/pages/ParabensPage.java
@@ -15,22 +15,25 @@ public class ParabensPage extends BaseSteps {
     private String xpathParabensNomeCliente = "//*[@data-automation='parabens-sucesso']";
     private String xpathParabensNomePlano = "//*[@data-automation='msg-sucesso-plano']";
     private String xpathParabensPedidoCliente = "//*[@data-automation='msg-sucesso-pedido']";
-    private String xpathNumeroDoPedido = "//*[@class='information-label order-label-title d-flex']";
-    private String xpathNomePedido = "(//*[@class='information-label d-flex'])[1]";
-    private String xpathCpfPedido = "(//*[@class='information-label d-flex'])[2]";
-    private String xpathTelefonePedido = "(//*[@class='information-label d-flex'])[3]";
-    private String xpathPlanoNomePedido = "(//*[@class='information-label d-flex'])[4]";
-    private String xpathValorPedido = "(//*[@class='information-label d-flex'])[5]";
-    private String xpathFormaPagamentoPedido = "(//*[@class='information-label d-flex'])[6]";
-    private String xpathDiaVencimentoFatura = "(//*[@class='information-label d-flex'])[7]";
-    private String xpathEnderecoDeEntregaPedido = "(//*[@class='information-label d-flex'])[8]";
+    private String xpathNumeroDoPedido = "(//*[@data-automation='informacao-pedido'])[2]";
+    private String xpathNomePedido = "//*[@data-automation='informacao-nome']";
+    private String xpathCpfPedido = "//*[@data-automation='informacao-cpf']";
+    private String xpathTelefonePedido = "//*[@data-automation='informacao-telefone']";
+    private String xpathPlanoNomePedido = "(//*[@data-automation='informacao-plano'])";
+    private String xpathValorPedido = "(//*[@data-automation='informacao-valor'])";
+    private String xpathFormaPagamentoPedido = "(//*[@data-automation='informacao-forma-pagamento'])";
+    private String xpathDiaVencimentoFatura = "(//*[@data-automation='informacao-vencimento'])";
+    private String xpathEnderecoDeEntregaPedido = "//*[@data-automation='endereco-conclusao']";
     private String xpathTipoDoFretePedido = "//*[@data-automation='tipo-frete-conclusao']";
     private String xpathValorDoFretePedido = "//*[@data-automation='valor-frete-conclusao']";
 
     public void validarCamposPedido() {
-        Assert.assertTrue(driver.getText(xpathParabensNomeCliente, "xpath").contains("Parabéns, " + DadosPessoaisPage.nomeCliente.split(" ")[0]));
+        if (Hooks.tagScenarios.contains("@aquisicao") || Hooks.tagScenarios.contains("@portabilidade") || Hooks.tagScenarios.contains("@migracaoPre")) {
+            driver.JavaScriptClick("(//*[@title='Acordion clicável para expandir o conteúdo'])[1]", "xpath");
+        }
+        Assert.assertTrue(driver.getText(xpathParabensNomeCliente, "xpath").contains("Parabéns, " + driver.capitalizeFirstLetter(DadosPessoaisPage.nomeCliente.split(" ")[0]) + "!"));
         Assert.assertTrue(driver.getText(xpathParabensNomePlano, "xpath").contains(HomePage.tituloCardHome) && driver.getText(xpathPlanoNomePedido, "xpath").contains(HomePage.tituloCardHome));
-        Assert.assertTrue(driver.getText(xpathParabensPedidoCliente, "xpath").contains(driver.getText(xpathNumeroDoPedido, "xpath").replaceAll("\\s", "").substring(14)));
+        Assert.assertTrue(driver.getText(xpathParabensPedidoCliente, "xpath").contains(driver.getText(xpathNumeroDoPedido, "xpath").replaceAll("\\s", "").substring(14).replaceAll("^0+","")));
         Assert.assertTrue(driver.getText(xpathNomePedido, "xpath").contains(DadosPessoaisPage.nomeCliente.toUpperCase()));
         Assert.assertTrue(driver.getText(xpathCpfPedido, "xpath").contains(driver.mascararCpf(CarrinhoPage.cpfCliente)));
         if (Hooks.tagScenarios.contains("@entregaExpressa")) {
@@ -39,7 +42,6 @@ public class ParabensPage extends BaseSteps {
         } else if (Hooks.tagScenarios.contains("@entregaConvencional")) {
             Assert.assertTrue(driver.getText(xpathTipoDoFretePedido, "xpath").contains(DadosPessoaisPage.tipoDeFreteCarrinho));
             Assert.assertTrue(driver.getText(xpathValorDoFretePedido, "xpath").contains(Character.toUpperCase(DadosPessoaisPage.valorDoFreteCarrinho.charAt(6)) + DadosPessoaisPage.valorDoFreteCarrinho.substring(7)));
-            Assert.assertTrue(driver.getText(xpathTelefonePedido, "xpath").contains(driver.mascararTelefone(CarrinhoPage.telefoneCliente)));
         } else {
             Assert.assertTrue(driver.getText(xpathTelefonePedido, "xpath").contains(driver.mascararTelefone(CarrinhoPage.telefoneCliente)));
         }
@@ -47,6 +49,7 @@ public class ParabensPage extends BaseSteps {
         Assert.assertTrue(driver.getText(xpathFormaPagamentoPedido, "xpath").contains(CustomizarFaturaPage.formaPagamentoPedidoCarrinho));
         if (Hooks.tagScenarios.contains("@aquisicao") || Hooks.tagScenarios.contains("@portabilidade") || Hooks.tagScenarios.contains("@migracaoPre")) {
             Assert.assertTrue(driver.getText(xpathDiaVencimentoFatura, "xpath").contains(CustomizarFaturaPage.dataVencimentoFatura));
+            driver.JavaScriptClick("(//*[@title='Acordion clicável para expandir o conteúdo'])[2]", "xpath");
             Assert.assertTrue(driver.getText(xpathEnderecoDeEntregaPedido, "xpath").contains(driver.montaEnderecoValidacaoParabens(DadosPessoaisPage.enderecoCliente, DadosPessoaisPage.numeroEndCliente, DadosPessoaisPage.complementoCliente, DadosPessoaisPage.bairroCliente, DadosPessoaisPage.cidadeCliente, DadosPessoaisPage.ufCliente, DadosPessoaisPage.cepCliente)));
         }
     }
