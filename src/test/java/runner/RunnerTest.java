a08a51904f5a66164a6ead466fb4d695675cad1a
danielsilva2 <daniel.silva0740@gmail.com>
2024-01-22 15:20:32 -0300

    masterFix - Branch para pequenos fixes na master como massas ou pontos específicos que interferem na execução dos testes

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -15,7 +15,7 @@ import support.BaseSteps;
         monochrome = false,
         dryRun = false,
         glue = {"steps", "support"},
-        tags = {"~@ignore", "@migracaoControleRentabilizacaoDA"})
+        tags = {"~@ignore", "@regressivo"})
 
 public class  RunnerTest extends BaseSteps {
 
