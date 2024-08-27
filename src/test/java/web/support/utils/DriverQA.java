5dea4b1aef49416ca87edf3a48761997c10c72c3
Nicolas Yoshioka <nicolas.yoshioka.3@globalhitss.com.br>
2024-08-27 12:17:44 -0300

    remove importacao inutilizadas

--- src/test/java/web/support/utils/DriverQA.java
+++ src/test/java/web/support/utils/DriverQA.java
@@ -5,8 +5,6 @@ import org.jsoup.nodes.Document;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
