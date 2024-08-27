2286950e92a06601fb1c3ef51d59c306a9719ae8
Nicolas Yoshioka <nicolas.yoshioka.3@globalhitss.com.br>
2024-08-27 12:18:51 -0300

    remover importacoes inutilizadas

--- src/test/java/web/support/utils/DriverQA.java
+++ src/test/java/web/support/utils/DriverQA.java
@@ -17,7 +17,6 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static java.time.Duration.ofSeconds;
 import static web.support.api.RestAPI.getEmailMessage;
