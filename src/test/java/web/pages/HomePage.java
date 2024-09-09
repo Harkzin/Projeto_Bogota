059884e9dc6512405688c1840f917ccbd385c6c4
Gustavo Bernardi <gustavo.nevesbernardi@everis.nttdata.com>
2024-09-09 15:43:14 -0300

    ECCMAUT351-Fix-pull-stage-HomePage

--- src/test/java/web/pages/HomePage.java
+++ src/test/java/web/pages/HomePage.java
@@ -31,55 +31,6 @@ public class HomePage {
 
     private boolean isDebit;
 
-    private void validarCardPlano(String code) {
-        //TODO atualizar find para id quando for criado
-        WebElement cardParent = driverQA.findElement("//*[@id='addToCartForm" + code + "']/../preceding-sibling::div[contains(@class, 'top-card')]/div", "xpath");
-
-        //Valida nome
-        if (!cartOrder.getPlan().getName().isEmpty()) {
-            WebElement planName = cardParent.findElement(By.xpath("h3"));
-            ComumPage.validateElementText(cartOrder.getPlan().getName(), planName);
-        }
-
-        //Valida preço
-        WebElement price = cardParent
-                .findElement(By.xpath("div[@data-price-for]/div/div[@class='preco-home bestPrice']/div/p[2]"));
-        Assert.assertEquals(cartOrder.getPlan().getFormattedPlanPrice(isDebit, true), price.getText().trim());
-        Assert.assertTrue(price.isDisplayed());
-
-        //Valida apps ilimitados
-        if (cartOrder.getPlan().hasPlanApps()) {
-            List<WebElement> planApps = cardParent
-                    .findElements(By.xpath("div[@class='characteristics']/div[@class='component-apps-ilimitados apps-ilimitados']//img"));
-            ComumPage.validarMidiasPlano(cartOrder.getPlan().getPlanApps(), planApps, driverQA);
-        }
-
-        //Valida título extraPlay
-        if (cartOrder.getPlan().hasExtraPlayTitle()) {
-            WebElement extraPlayTitle = cardParent
-                    .findElement(By.xpath("div[@class='characteristics']/div[contains(@class, 'title-extra-play')][1]/p"));
-            ComumPage.validateElementText(cartOrder.getPlan().getExtraPlayTitle(), extraPlayTitle);
-        }
-
-        //Valida apps extraPlay
-        if (cartOrder.getPlan().hasExtraPlayApps()) {
-            List<WebElement> extraPlayApps = cardParent
-                    .findElements(By.xpath("div[@class='characteristics']/div[contains(@class, 'component-apps-ilimitados extra-play')]//img"));
-            ComumPage.validarMidiasPlano(cartOrder.getPlan().getExtraPlayApps(), extraPlayApps, driverQA);
-        }
-
-        //Valida planPortability (GB e bônus - antigo)
-        if (cartOrder.getPlan().hasPlanPortability()) {
-            List<WebElement> planPortability = cardParent
-                    .findElements(By.xpath("div[@class='characteristics']/div[contains(@class, 'title-extra-play')]"))
-                    .stream()
-                    .map(webElement -> webElement.findElement(By.tagName("p")))
-                    .collect(Collectors.toList());
-
-            ComumPage.validarPlanPortability(planPortability, cartOrder.getPlan());
-        }
-    }
-
     public void acessarLojaHome() {
         driverQA.getDriver().get(Constants.urlAmbiente);
         driverQA.waitPageLoad(Constants.urlAmbiente, 20);
