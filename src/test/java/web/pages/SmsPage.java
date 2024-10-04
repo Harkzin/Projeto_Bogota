683d684c01232451be8be93aa474dd48e5b2de2d
gabriel.darbone.glo <gabriel.darbone.3@globalhitss.com.br>
2024-10-04 15:25:51 -0300

    adding scenario

--- src/test/java/web/pages/SmsPage.java
+++ src/test/java/web/pages/SmsPage.java
@@ -19,23 +19,35 @@ public class SmsPage {
         this.driverWeb = driverWeb;
     }
 
-    private WebElement token;
+    private WebElement tokenField1;
+    private WebElement tokenField2;
+    private WebElement tokenField3;
+    private WebElement tokenField4;
+
 
     public void validarPaginaSms() {
         driverWeb.waitPageLoad("/checkout/multi/summary/view", 15);
 
-        token = driverWeb.findElement("txt-token", "id");
-        Assert.assertEquals(token.getAttribute("value"), "");
+        tokenField1 = driverWeb.findElement("txt-token1", "id");
+        tokenField2 = driverWeb.findElement("txt-token2", "id");
+        tokenField3 = driverWeb.findElement("txt-token3", "id");
+        tokenField4 = driverWeb.findElement("txt-token4", "id");
+
+        Assert.assertEquals(tokenField1.getAttribute("value"), "");
+        Assert.assertEquals(tokenField2.getAttribute("value"), "");
+        Assert.assertEquals(tokenField3.getAttribute("value"), "");
+        Assert.assertEquals(tokenField4.getAttribute("value"), "");
+
     }
 
     public void inserirToken() {
         String htmlComment = ((JavascriptExecutor) driverWeb.getDriver()).executeScript("return document.documentElement.lastChild").toString();
         int tokenStartIndex = htmlComment.indexOf("TOKEN_SEND_TO_CLIENT") + 25;
 
-        driverWeb.sendKeys(token, htmlComment.substring(tokenStartIndex, tokenStartIndex + 4));
+        driverWeb.sendKeys(tokenField1, htmlComment.substring(tokenStartIndex, tokenStartIndex + 4));
     }
 
     public void clicarFinalizar() {
-        driverWeb.javaScriptClick("btn-finalizar", "id");
+        driverWeb.javaScriptClick("btn-confirmar", "id");
     }
 }
