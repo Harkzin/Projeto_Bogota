58cc0eb159daffc4cd5ea6def21552874c4474af
Lucas <lucas.santos.3@globalhitss.com.br>
2024-11-22 14:09:09 -0300

    feat/ECCMAUT-1464: Adicionado funcionalidade para buscar Token do WOA e dependencias

---
+++ src/test/java/web/support/token/database/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package src.test.java.web.support.token.database;
+
+public class DatabaseConfig {
+    private String jdbcUrl;
+    private String username;
+    private String password;
+
+    // Getters e Setters
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
