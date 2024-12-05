9b22d5dcea2bb9a2866bb8a6202fabdf145f896e
Gustavo Bernardi <gustavo.nevesbernardi@everis.nttdata.com>
2024-12-05 13:40:47 -0300

    ECCMAUT1452-valida-parabens-dep

--- src/test/java/web/pages/CarrinhoPage.java
+++ src/test/java/web/pages/CarrinhoPage.java
@@ -237,15 +237,19 @@ public class CarrinhoPage {
         driverWeb.sendKeys(cpfAquisicao, getCpfForPixFlow());
     }
 
-    public void inserirDadosAquisicao(String telefoneContato, boolean cpfAprovado, boolean cpfDiretrix) {
+    public String inserirDadosAquisicao(String telefoneContato, boolean cpfAprovado, boolean cpfDiretrix) {
+        String cpf = getCpfForPlanFlow(cpfAprovado, cpfDiretrix);
         driverWeb.sendKeys(telefoneContatoAquisicao, telefoneContato);
-        driverWeb.sendKeys(cpfAquisicao, getCpfForPlanFlow(cpfAprovado, cpfDiretrix));
+        driverWeb.sendKeys(cpfAquisicao, cpf);
+
+        return cpf;
     }
 
-    public void inserirEmail() {
+    public String inserirEmail() {
         String userEmail = UUID.randomUUID().toString().replace("-", "") + "@mailsac.com";
-        cartOrder.getUser().setEmail(userEmail);
         driverWeb.sendKeys(this.email, userEmail);
+
+        return userEmail;
     }
 
     public void clicarEuQuero() {
--- src/test/java/web/pages/ComumPage.java
+++ src/test/java/web/pages/ComumPage.java
@@ -218,7 +218,7 @@ public class ComumPage {
                 String depQuantityRef = String.format("+ %d %s", depQtt, depQtt == 1 ? "dependente" : "dependentes");
                 validateElementText(depQuantityRef, driverWeb.findByXpath(planContentParent + "//*[@data-plan-content='dependentquantity']"));
 
-                String depPriceAndChipRef = String.format("R$ %s ( + %d %s)", formatPrice(depQtt * DEPENDENT_PRICE), depQtt, depQtt == 1 ? "chip para dependente" : "chips para dependentes");
+                String depPriceAndChipRef = String.format("R$ %s (+ %d %s)", formatPrice(depQtt * DEPENDENT_PRICE), depQtt, depQtt == 1 ? "chip para dependente" : "chips para dependentes");
                 validateElementText(depPriceAndChipRef, driverWeb.findByXpath(planContentParent + "//*[@data-plan-content='dependentprice']"));
             }
 
