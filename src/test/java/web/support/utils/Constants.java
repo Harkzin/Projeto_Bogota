c674311322b8e2bf94876fc7b4a1a7daa50bafd7
dsilvnas <daniel.silvanascimento@nttdata.com>
2024-12-18 16:13:44 -0300

    ajustes constants

--- src/test/java/web/support/utils/Constants.java
+++ src/test/java/web/support/utils/Constants.java
@@ -82,7 +82,7 @@ public final class Constants {
         ACQUISITION_PLAN_EXPRESS(List.of("Pedido realizado", "Pedido em análise", "Pedido aprovado e em separação", "Pedido concluído")),
         ACQUISITION_DEVICE(List.of("Pedido recebido", "Pedido em análise", "Pedido aprovado e em separação", "Pedido faturado", "Pedido em transporte", "Pedido entregue e ativado")),
         MIGRATE_EXCHANGE_PLAN(List.of("Pedido realizado", "Pedido em análise", "Ativação do Plano")),
-        MIGRATE_EXCHANGE_DEVICE(List.of()),
+        MIGRATE_EXCHANGE_DEVICE(List.of("Pedido recebido", "Pedido em análise", "Pedido aprovado e em separação", "Pedido faturado", "Pedido em transporte", "Pedido entregue e ativado")),
         PORTABILITY_PLAN(List.of("Pedido realizado", "Pedido em análise", "Pedido aprovado e em separação", "Pedido a caminho", "Pedido entregue", "Linha provisória ativada", "Portabilidade concluída")),
         PORTABILITY_DEVICE(List.of("Pedido recebido", "Pedido em análise", "Pedido aprovado e em seperação", "Pedido faturado", "Pedido em transporte", "Pedido entregue e ativado")),
         ACCESSORY(List.of());
